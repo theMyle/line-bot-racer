872d6267b85a43476b191eec814e9b8d707619fd
theMyle <99082612+theMyle@users.noreply.github.com>
2025-11-17 23:14:29 +0800

    motor headers removed abstract classes usage

--- include/motor/MotorPair.h
+++ include/motor/MotorPair.h
@@ -1,38 +1,51 @@
 
 #include <stdint.h>
-#include <motor/IMotorPair.h>
-#include <motor/IMotor.h>
+#include <motor/Motor.h>
 
 #ifndef MOTOR_PAIR_H
 #define MOTOR_PAIR_H
 
-class MotorPair : public IMotorPair
+class MotorPair
 {
 private:
-    IMotor &leftMotor;
-    IMotor &rightMotor;
+    Motor leftMotor;
+    Motor rightMotor;
 
 public:
-    MotorPair(IMotor &left, IMotor &right)
-        : leftMotor(left), rightMotor(right) {};
+    // constructs motor pair from two individual motor configs (PINS).
+    //
+    // first 4 params - LEFT_MOTOR {PWM, IN1, IN2, IS_INVERTED_FLAG}.
+    // last 4 params - RIGHT_MOTOR {PWM, IN1, IN2, IS_INVERTED_FLAG}.
+    MotorPair(
+        uint8_t l_pwm, uint8_t l_in1, uint8_t l_in2, bool l_inverted,
+        uint8_t r_pwm, uint8_t r_in1, uint8_t r_in2, bool r_inverted)
+        : leftMotor(l_pwm, l_in1, l_in2, l_inverted),
+          rightMotor(r_pwm, r_in1, r_in2, r_inverted)
+    {
+    }
 
     inline void forward(uint8_t speed)
     {
         leftMotor.forward(speed);
         rightMotor.forward(speed);
-    };
+    }
 
     inline void backward(uint8_t speed)
     {
-        leftMotor.forward(speed);
+        leftMotor.backward(speed);
         rightMotor.backward(speed);
-    };
+    }
 
     inline void stop()
     {
         leftMotor.stop();
         rightMotor.stop();
     }
+
+    inline void turnLeft();
+    inline void turnRight();
+    inline void turn180();
+    inline void spin();
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,40 +5,38 @@
 
 constexpr uint8_t SENSOR_COUNT = 8;
 constexpr uint8_t SENSOR_PINS[SENSOR_COUNT] = {2, 3, 4, 5, 6, 7, 8, 9};
-
 QTRSensors qtr;
 uint16_t sensorValues[SENSOR_COUNT];
 
-void setup()
+void calibrateSensors(uint8_t steps)
 {
-  Serial.begin(9600);
-
-  qtr.setTypeRC(); // digital
-  qtr.setSensorPins(SENSOR_PINS, SENSOR_COUNT);
-
-  Serial.println("Calibrating...");
-  for (auto i = 0; i < 150; i++)
+  Serial.println("\nCalibrating\n");
+  for (auto i = 0; i < steps; i++)
   {
     qtr.calibrate();
     delay(20);
     Serial.print(".");
   }
-  Serial.println("\nDone calbrating!!");
-  delay(1000);
+  Serial.println("\nCalibration Finished!");
+}
 
-  // sample motor setup
-  // Motor left = Motor(1, 2, 3, 4);
-  // Motor right = Motor(1, 2, 3, 4);
-  // MotorPair Motors = MotorPair(left, right);
+void setup()
+{
+  Serial.begin(9600);
+
+  // motor setup
+  // MotorPair Motors = MotorPair(1, 1, 1, 1, 2, 3, 4, 5);
+
+  // sensor setup
+  qtr.setTypeRC(); // digital
+  qtr.setSensorPins(SENSOR_PINS, SENSOR_COUNT);
+  calibrateSensors(150);
+
+  // other stuff
 }
 
 void loop()
 {
-  // for (auto i = 0; i < SENSOR_COUNT; i++)
-  // {
-  //   sensorValues[i] = digitalRead(SENSOR_PINS[i]);
-  // }
-
   qtr.readLineBlack(sensorValues);
 
   for (auto o : sensorValues)
